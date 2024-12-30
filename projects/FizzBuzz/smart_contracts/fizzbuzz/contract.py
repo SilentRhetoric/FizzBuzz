59f92a874970a152a34f86e956d6748e37a36ae5
SilentRhetoric <dev@silentrhetoric.com>
2024-12-30 14:26:32 -0500

    FizzBuzz solved!

--- projects/FizzBuzz/smart_contracts/fizzbuzz/contract.py
+++ projects/FizzBuzz/smart_contracts/fizzbuzz/contract.py
@@ -1,8 +1,33 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
-
+from algopy import ARC4Contract, Bytes, OpUpFeeSource, ensure_budget, subroutine, UInt64, String, urange, arc4
 
 class Fizzbuzz(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    @arc4.abimethod()
+    def fizzbuzz(self) -> arc4.DynamicArray[arc4.String]:
+        result = arc4.DynamicArray[arc4.String]()
+        for n in urange(100):
+            # Need to do OpUps to get budget for the next iteration of the loop
+            ensure_budget(2550, OpUpFeeSource.AppAccount)
+            result.append(arc4.String(self.divide(n + 1))) # Start from 1 not 0
+        return result
+    
+    # The core FizzBuzz algorithm logic
+    @subroutine
+    def divide(self, number: UInt64) -> String:
+        if number % 3 == 0 and number % 5 == 0:
+            return String("FizzBuzz")
+        elif number % 3 == 0:
+            return String("Fizz")
+        elif number % 5 == 0:
+            return String("Buzz")
+        else:
+            return itoa(number)
+
+# Utility to convert integers-->strings so they can be added to the result array
+@subroutine
+def itoa(int: UInt64) -> String:
+    digits = Bytes(b"0123456789")
+    radix = digits.length
+    if int < radix:
+        return String.from_bytes(digits[int])
+    return itoa(int // radix) + String.from_bytes(digits[int % radix])
+
--- projects/FizzBuzz/smart_contracts/fizzbuzz/deploy_config.py
+++ projects/FizzBuzz/smart_contracts/fizzbuzz/deploy_config.py
@@ -1,13 +1,13 @@
 import logging
-
 import algokit_utils
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+from algokit_utils.config import config
 
+config.configure(debug=True, trace_all=True)
 logger = logging.getLogger(__name__)
 
-
-# define deployment behaviour based on supplied app spec
+# Define deployment behaviour based on supplied app spec
 def deploy(
     algod_client: AlgodClient,
     indexer_client: IndexerClient,
@@ -24,13 +24,26 @@ def deploy(
         indexer_client=indexer_client,
     )
 
-    app_client.deploy(
+    deploy_result = app_client.deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
     )
-    name = "world"
-    response = app_client.hello(name=name)
+
+    # Ensure the deployed app is funded to send inner txns for opcode budget
+    algokit_utils.ensure_funded(
+        algod_client,
+        algokit_utils.EnsureBalanceParameters(
+            account_to_fund=deploy_result.app.app_address,
+            min_spending_balance_micro_algos=1_000_000,
+            min_funding_increment_micro_algos=100_000,
+        ),
+    )
+
+    # Where the app call actually occurs--so terse!
+    response = app_client.fizzbuzz()
+
     logger.info(
-        f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+        f"Called app {app_spec.contract.name} ({app_client.app_id})\n"
+        f"Received return value: {response.return_value}\n"
+        f"App call txn ID: {response.tx_id}"
     )
